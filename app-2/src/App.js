bb3554ddc05eedfe4b15b51d61ca3272351d27ee
robert hunnicutt <robhunn74@gmail.com>
2018-11-10 20:45:57 -0600

    push

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+ 
 class App extends Component {
   render() {
     const showArray = ['hello','world','map','array','show','on screen']
