8c49eba4b0aaf54db0e7d2325121ba2ed27c1402
robert hunnicutt <robhunn74@gmail.com>
2018-11-22 19:13:00 -0600

    push

--- app-1/src/index.js
+++ app-1/src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './App1';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+
+<App />
+
+, document.getElementById('root'));
 registerServiceWorker();
--- app-3/src/App.js
+++ app-3/src/App.js
@@ -4,17 +4,17 @@ import './App.css';
 class App extends Component {
   state = {
     filterString:'',
-    list:["filter",'through','this','list','thanks']
+    list:["filter",'through','this','list','thanks', '222','22', '2222','22222']
   }
-  handleChange2=(filter)=>{
-    this.setState({filterString : filter})
+  handleChange2 = ( filter ) => {
+    this.setState({ filterString : filter })
   }
   render() {
-    let looper = this.state.list.filter((e,i)=>{
-      return e.includes(this.state.filterString);
-    }).map( (e,i) => {
-      return <h3 key={i}> {e} </h3>
-    })
+    let looper = this.state.list.filter(( e, i ) => {
+      return e.includes( this.state.filterString );
+    }).map( ( e,i ) => {
+      return <h3 key = { i }> { e  } </h3>
+    }) 
     return (
       <div className="App">
         <div className="App-header">
