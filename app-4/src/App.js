6408c3602ff53cd331b27ba3f216143497343d0d
robert hunnicutt <robhunn74@gmail.com>
2018-12-05 13:18:06 -0600

    push

--- app-4/src/App.js
+++ app-4/src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
+        <div className="App-header"> 
             <Login/>
         </div>
       </div>
