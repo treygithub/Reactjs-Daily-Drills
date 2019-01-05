60ab7c1ec7b46718fd13251bfe9ef9567e7bf0d6
robert hunnicutt <robhunn74@gmail.com>
2019-01-04 21:41:13 -0600

    push

--- app-5/src/App.js
+++ app-5/src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Image Myimage={ "./Myimage.jpg" } />
+        <Image Myimage={ "./Myimage.jpg" } /> 
       </div>
     );
   }
